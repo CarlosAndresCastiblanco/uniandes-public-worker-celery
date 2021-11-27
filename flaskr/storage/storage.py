2d61a2a9dfe4ae89db3c9908e3f4d6b819787010
Carlos Andrés Castiblanco Castro <c.castiblancoc@uniandes.edu.co>
2021-11-27 04:33:10 -0500

    :construction: CC Fix

--- flaskr/storage/storage.py
+++ flaskr/storage/storage.py
@@ -167,24 +167,24 @@ def receive_and_delete_messages_queue():
         if find_object(sso_bucket_s3, sso_region,
                        "origin-{}-{}.{}".format(author, title, body.split(",")[0])):
             downloading_files(
-                "./originales/{}".format("origin-{}-{}.{}".format(author, title, body.split(",")[0])),
+                "../originales/{}".format("origin-{}-{}.{}".format(author, title, body.split(",")[0])),
                 sso_bucket_s3,
                 "origin-{}-{}.{}".format(author, title, body.split(",")[1]),
                 sso_region
             )
             archivo = AudioSegment.from_file(
-                "./originales/origin-{}-{}.{}".format(author, title, body.split(",")[0]),
+                "../originales/origin-{}-{}.{}".format(author, title, body.split(",")[0]),
                 body.split(",")[0])
             archivo.export(
-                "./originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]),
+                "../originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]),
                 format=body.split(",")[1])
             print('convertido satisfactoriamente',
                   "destino-{}-{}.{}".format(author, title, body.split(",")[1]))
-            upload_file("./originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]),
+            upload_file("../originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]),
                         sso_bucket_s3,
                         "destino-{}-{}.{}".format(author, title, body.split(",")[1]),
                         sso_region)
-            remove_file("./originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]))
+            remove_file("../originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]))
             update_processed(title)
         else:
             print("Archivo no encontrado en S3")
