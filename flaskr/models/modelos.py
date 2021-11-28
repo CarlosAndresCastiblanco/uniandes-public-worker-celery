a0a8bae0d0b8fd011863ab3a07574dacc556115a
Carlos Andrés Castiblanco Castro <c.castiblancoc@uniandes.edu.co>
2021-11-27 22:01:09 -0500

    :construction: CC Fix

--- flaskr/models/modelos.py
+++ flaskr/models/modelos.py
@@ -43,10 +43,7 @@ def find_conversion_each_in_progress():
     return session.query(Conversion).filter(Conversion.estado.in_(['uploaded'])).first()
 
 def update_processed(id_conversion):
-    print("In update_processed "+id_conversion)
     c = session.query(Conversion).filter_by(id=id_conversion).first()
-    #conversion = session.query(Conversion).get(id_conversion)
-    print("conversion ",str(c))
     c.estado = "processed"
     session.commit()
 
--- flaskr/storage/storage.py
+++ flaskr/storage/storage.py
@@ -13,74 +13,50 @@ origin_folder = os.getenv('ORIGIN_FOLDER')
 
 AudioSegment.converter = which("ffmpeg")
 
+
 def upload_file(file_name, bucket, object_name, region):
-    print("In upload_file")
-    s3_client = boto3.client('s3',
-                             region_name=region,
-                             aws_access_key_id='AKIAVI7PUQMWA7CHFW7Q',
-                             aws_secret_access_key='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')
+    s3_client = boto3.client('s3', region_name=region)
     try:
         s3_client.upload_file(file_name, bucket, object_name)
-        print('upload_file is good')
     except ClientError as e:
         logging.error(e)
         return False
     return True
 
+
 def downloading_files(file_name, bucket, object_name, region):
-    print("In downloading_files")
-    s3 = boto3.client('s3',
-                      region_name=region,
-                      aws_access_key_id='AKIAVI7PUQMWA7CHFW7Q',
-                      aws_secret_access_key='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')
+    s3 = boto3.client('s3', region_name=region)
     try:
         s3.download_file(bucket, object_name, file_name)
-        print('downloading_files is good')
     except ClientError as e:
         logging.error(e)
         return False
     return True
+
 
 def get_object_name(file_name):
     return os.path.basename(file_name)
 
+
 def remove_file(file_name):
-    print('In remove_file delete ' + file_name)
     if os.path.exists(file_name):
         os.remove(file_name)
-        print('Archivo removido con exito ' + file_name)
     else:
-        print('No se pudo remover el archivo')
         return 'No se pudo remover el archivo', 500
 
 
 def find_object(bucket, region, object_name):
-    print("In find_object")
-    """
-    s3 = boto3.resource('s3',
-                        region_name=region,
-                        aws_access_key_id='AKIAVI7PUQMWA7CHFW7Q',
-                        aws_secret_access_key='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')
-    """
-    s3 = boto3.resource('s3',
-                        region_name=region)
-
+    s3 = boto3.resource('s3', region_name=region)
     bucket = s3.Bucket(bucket)
-    print("I need to find......... " + object_name)
     a = [x for x in bucket.objects.all() if x.key == object_name]
     if len(a) > 0:
-        print("in find_object..... True")
         return True
     else:
-        print("in find_object..... False")
         return False
 
 
 def delete_object(bucket, region, object_name):
-    s3 = boto3.resource('s3',
-                        region_name=region,
-                        aws_access_key_id='AKIAVI7PUQMWA7CHFW7Q',
-                        aws_secret_access_key='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')
+    s3 = boto3.resource('s3', region_name=region)
     try:
         s3.Object(bucket, object_name).delete()
     except Exception as e:
@@ -91,7 +67,6 @@ def delete_object(bucket, region, object_name):
 
 def receive_and_delete_messages_queue():
     sqs = boto3.client('sqs', region_name=sso_region)
-
     response = sqs.receive_message(
         QueueUrl=queue_url,
         AttributeNames=[
@@ -106,89 +81,78 @@ def receive_and_delete_messages_queue():
     )
     message = response['Messages'][0]
     receipt_handle = message['ReceiptHandle']
-
     body = message['Body']
-    print("body......... " + body)
-
     title = message['MessageAttributes']['Title']['StringValue']
-    print("title......... " + title)
-
     author = message['MessageAttributes']['Author']['StringValue']
-    print("author......... " + author)
-
     origen = body.split(",")[0]
-    print("First..." + origen)
-
     destino = body.split(",")[1]
-    print("Second..." + destino)
 
     try:
-        if find_object(sso_bucket_s3, sso_region,
-                       "origin-{}-{}.{}".format(author, title, body.split(",")[0])):
+        if find_object(sso_bucket_s3, sso_region, "origin-{}-{}.{}".format(author, title, origen)):
             downloading_files(
-                'originales/{}'.format("origin-{}-{}.{}".format(author, title, body.split(",")[0])),
+                'originales/{}'.format("origin-{}-{}.{}".format(author, title, origen)),
                 sso_bucket_s3,
-                "origin-{}-{}.{}".format(author, title, body.split(",")[0]),
+                "origin-{}-{}.{}".format(author, title, origen),
                 sso_region
             )
             archivo = AudioSegment.from_file(
-                "originales/origin-{}-{}.{}".format(author, title, body.split(",")[0]),
-                body.split(",")[0])
-            print("AudioSegment.from_file")
+                "originales/origin-{}-{}.{}".format(author, title, origen),
+                origen
+            )
             archivo.export(
-                "originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]),
-                format=body.split(",")[1])
-            remove_file("originales/origin-{}-{}.{}".format(author, title, body.split(",")[0]))
-            print('convertido satisfactoriamente ' +
-                  "destino-{}-{}.{}".format(author, title, body.split(",")[1]))
-            upload_file("originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]),
-                        sso_bucket_s3,
-                        "destino-{}-{}.{}".format(author, title, body.split(",")[1]),
-                        sso_region)
-            remove_file("originales/destino-{}-{}.{}".format(author, title, body.split(",")[1]))
+                "originales/destino-{}-{}.{}".format(author, title, destino),
+                format=destino
+            )
+            remove_file("originales/origin-{}-{}.{}".format(author, title, origen))
+            upload_file(
+                "originales/destino-{}-{}.{}".format(author, title, destino),
+                sso_bucket_s3,
+                "destino-{}-{}.{}".format(author, title, destino),
+                sso_region
+            )
+            remove_file("originales/destino-{}-{}.{}".format(author, title, destino))
             update_processed(title)
             sqs.delete_message(
                 QueueUrl=queue_url,
                 ReceiptHandle=receipt_handle
             )
         else:
-            print("Archivo no encontrado en S3")
-    except Exception as err:
-        print('error convirtiendo')
-        print(err)
-        print(err.args)
+            logging.error("Archivo no encontrado en S3")
+    except Exception as e:
+        logging.error('error convirtiendo')
+        logging.error(e)
 
 
 def conversion_background(conversion):
-
     try:
         if find_object(sso_bucket_s3, sso_region,
                        "origin-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.origen)):
             downloading_files(
-                'originales/{}'.format("origin-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.origen)),
+                'originales/{}'.format(
+                    "origin-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.origen)),
                 sso_bucket_s3,
                 "origin-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.origen),
                 sso_region
             )
             archivo = AudioSegment.from_file(
                 "originales/origin-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.origen),
-                conversion.origen)
-            print("AudioSegment.from_file")
+                conversion.origen
+            )
             archivo.export(
                 "originales/destino-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.destino),
-                format=conversion.destino)
+                format=conversion.destino
+            )
             remove_file("originales/origin-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.origen))
-            print('convertido satisfactoriamente ' +
-                  "destino-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.destino))
-            upload_file("originales/destino-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.destino),
-                        sso_bucket_s3,
-                        "destino-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.destino),
-                        sso_region)
+            upload_file(
+                "originales/destino-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.destino),
+                sso_bucket_s3,
+                "destino-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.destino),
+                sso_region
+            )
             remove_file("originales/destino-{}-{}.{}".format(conversion.usuario_id, conversion.id, conversion.destino))
             update_processed(str(conversion.id))
         else:
-            print("Archivo no encontrado en S3")
-    except Exception as err:
-        print('error convirtiendo')
-        print(err)
-        print(err.args)
+            logging.error("Archivo no encontrado en S3")
+    except Exception as e:
+        logging.error('error convirtiendo')
+        logging.error(e)
